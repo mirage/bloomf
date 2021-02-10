8ebb80484f66a3dc2de9a23e046395e84732fb5c
Clément Pascutto <clement@pascutto.fr>
2021-02-10 13:09:06 +0100

    Add basic tests for union and inter

--- test/main.ml
+++ test/main.ml
@@ -20,35 +20,36 @@ let create_and_fill size =
   let elts = loop size StringSet.empty in
   (bf, elts)
 
-let bf, elts = create_and_fill 10_000
-
-(* The tests *)
 let test_mem () =
-  StringSet.iter
-    (fun r -> Alcotest.(check bool) "mem (present)" true (Bloomf.mem bf r))
-    elts
-
-let test_mem_create () =
-  let bf = Bloomf.create 10_000 in
-  StringSet.iter
-    (fun r -> Alcotest.(check bool) "mem (empty)" false (Bloomf.mem bf r))
-    elts
+  let sizes = [ 1_000; 10_000; 100_000 ] in
+  List.iter
+    (fun i ->
+      let bf, elts = create_and_fill i in
+      StringSet.iter
+        (fun r -> Alcotest.(check bool) "mem (empty)" true (Bloomf.mem bf r))
+        elts)
+    sizes
 
 let test_errors () =
-  let attempts = 100_000 in
-  let rec loop i count =
-    if i = 0 then count
-    else
-      let r = random_string 1024 in
-      if StringSet.mem r elts then loop i count
-      else loop (i - 1) (if Bloomf.mem bf r then count + 1 else count)
-  in
-  let count = loop attempts 0 in
-  let error_rate = float_of_int count /. float_of_int attempts in
-  if error_rate > 1.15 *. expected_error_rate then
-    Alcotest.failf "error_rate: expecting@\n%f, got@\n%f" expected_error_rate
-      error_rate
-  else ()
+  let sizes = [ 1_000; 10_000; 100_000 ] in
+  List.iter
+    (fun i ->
+      let bf, elts = create_and_fill i in
+      let attempts = 100_000 in
+      let rec loop i count =
+        if i = 0 then count
+        else
+          let r = random_string 1024 in
+          if StringSet.mem r elts then loop i count
+          else loop (i - 1) (if Bloomf.mem bf r then count + 1 else count)
+      in
+      let count = loop attempts 0 in
+      let error_rate = float_of_int count /. float_of_int attempts in
+      if error_rate > 1.15 *. expected_error_rate then
+        Alcotest.failf "error_rate: expecting@\n%f, got@\n%f"
+          expected_error_rate error_rate
+      else ())
+    sizes
 
 let test_size () =
   let sizes = [ 1_000; 10_000; 100_000 ] in
@@ -60,15 +61,32 @@ let test_size () =
         Alcotest.failf "size_estimate: expecting@\n%d, got@\n%d" i len)
     sizes
 
-let test_set =
+let test_op msg bop sop =
+  let sizes = [ 1_000; 10_000; 100_000 ] in
+  List.iter
+    (fun i ->
+      let bf1, elts1 = create_and_fill i in
+      let bf2, elts2 = create_and_fill i in
+      let bf3 = bop bf1 bf2 in
+      let elts3 = sop elts1 elts2 in
+      StringSet.iter
+        (fun r -> Alcotest.(check bool) msg true (Bloomf.mem bf3 r))
+        elts3)
+    sizes
+
+let test_union () = test_op "union" Bloomf.union StringSet.union
+
+let test_inter () = test_op "intersection" Bloomf.inter StringSet.inter
+
+let suite =
   [
     ("Mem returns true when element was added", `Quick, test_mem);
-    ("Mem returns false when filter is empty", `Quick, test_mem_create);
     ( "False positive rate is as specified (15% error allowed)",
       `Slow,
       test_errors );
     ("Size estimate is correct", `Slow, test_size);
+    ("Union", `Quick, test_union);
+    ("Intersection", `Quick, test_inter);
   ]
 
-(* Run it *)
-let () = Alcotest.run "Bloomf" [ ("bloomf", test_set) ]
+let () = Alcotest.run "Bloomf" [ ("bloomf", suite) ]
